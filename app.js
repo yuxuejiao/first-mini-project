759fe7df0b467c1bab6e743c8a65463d7d528d94
Elsa Yu <Elsa.Yu@nike.com>
2019-01-16 21:08:43 +0800

    update

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
-    // 获取用户信息
+    // 查看是否授权
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
